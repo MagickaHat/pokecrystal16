vulcandth/CrystalShireEngine

Resolve crash when level evolving a pokemon
Resolves #23

engine/pokemon/evolve.asm
@@ -199,9 +199,9 @@ EvolveAfterBattle_MasterLoop:
 	ld b, a
 	ld a, [wTempMonLevel]
 	cp b
-	jmp c, .skip_half_species_parameter
+	jmp c, .skip_evolution_species
 	call IsMonHoldingEverstone
-	jmp z, .skip_half_species_parameter
+	jmp z, .skip_evolution_species
 
 .proceed
 	ld a, [wTempMonLevel]

MagickaHat/pokecrystal16

engine/pokemon/evolve.asm
@@ -199,9 +199,9 @@ EvolveAfterBattle_MasterLoop:
 	ld b, a
 	ld a, [wTempMonLevel]
 	cp b
-	jp c, .skip_half_species_parameter
+	jp c, .skip_evolution_species
 	call IsMonHoldingEverstone
-	jp z, .skip_half_species_parameter
+	jp z, .skip_evolution_species
 
 .proceed
 	ld a, [wTempMonLevel]